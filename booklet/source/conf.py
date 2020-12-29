3c11060b20a25f481a1e1f6cdedded489ce895ae
Massimo Nocentini <massimo.nocentini@gmail.com>
2020-12-29 18:51:20 +0100

    Update the configuration to point to versioned json exported content from Pharo images.

--- booklet/source/conf.py
+++ booklet/source/conf.py
@@ -37,6 +37,7 @@ extensions = [
 
 # The following configuation values concerns the Pharo domain.
 pharo_json_export_filename='/Users/mn/Developer/snapshots/pharoes/90-microkanren/microkanren.json'
+pharo_json_export_filename='source/microkanren.json'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
